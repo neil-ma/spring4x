f3048947434e31f3c6b337e7ef47b16c19fefd1e
maning14 <maning14@meituan.com>
2019-05-24 17:20:08 +0800

    动态代理

---
+++ ch07/src/main/java/com/smart/proxy/ForumService.java
@@ -0,0 +1,13 @@
+package com.smart.proxy;
+
+/**
+ * @Title：ForumService
+ * @Description:
+ * @Author: natty
+ * @Version: 1.0
+ * @Date: 2019-05-24 17:19
+ */
+public interface ForumService {
+    void removeTopic(int topicId);
+    void removeForum(int forumId);
+}
